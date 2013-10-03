881434f02be385f0cfeb89cb9f5206d42018096a
jesse-browne <jb@greenpag.es>
2013-10-03 12:58:50 -0700

    Defined models for Idea and Upvotes, initialized dev db

--- steal_my_idea/ideas/models.py
+++ steal_my_idea/ideas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Define models.
+
+class Idea(models.Model):
+    HELP = 'HE'
+    EXECUTION = 'EX'
+    MONEY = 'MO'
+    AUTHOR_WANTS_OPTIONS = (
+        (HELP, 'Help! Work with me to make this idea a reality'),
+        (EXECUTION, 'Someone, anyone to make take this idea and make it a reality'),
+        (MONEY, 'Money to take this idea and turn it into a reality'),            
+    )
+    
+    title = models.CharField(max_length=200)
+    date_published = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    author_wants = models.CharField(max_length=2, choices=AUTHOR_WANTS_OPTIONS, default=EXECUTION)
+    
+class UpVotes(models.Model):
+    idea = models.ForeignKey(Idea)
+    votes = models.IntegerField(default=0)
+
