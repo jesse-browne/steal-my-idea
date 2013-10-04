171f4ea09c62d15dc8d6ffc1090a43e696b340aa
jesse-browne <jb@greenpag.es>
2013-10-04 15:32:50 -0700

    Re wrote ideas index view

--- steal_my_idea/ideas/views.py
+++ steal_my_idea/ideas/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from ideas.models import Idea
 
 def index(request):
     latest_idea_list = Idea.objects.order_by('-date_published')[:3]
-    template = loader.get_template('ideas/index.html')
-    context = RequestContext(request, {
-        'latest_idea_list': latest_idea_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_idea_list': latest_idea_list}
+    return render(request, 'ideas/index.html', context)
 
 def home(request):
     return HttpResponse('Home of Ideas to Steal!')
