85a0ffb59bad99ed755c71ce2cc85708366c462e
jesse-browne <jb@greenpag.es>
2013-10-03 19:30:10 -0700

    Began customizing admin panel

--- steal_my_idea/ideas/admin.py
+++ steal_my_idea/ideas/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from ideas.models import Idea
 
-admin.site.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_published')
+    list_filter = ['date_published']
+    search_fields = ['title']
+    date_hierarchy = 'date_published'
+
+admin.site.register(Idea, IdeaAdmin)
--- steal_my_idea/steal_my_idea/settings.py
+++ steal_my_idea/steal_my_idea/settings.py
@@ -111,6 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/users/gpadmin/sites/smi/steal_my_idea/templates' # development path, don't forget to add production!
 )
 
 INSTALLED_APPS = (
