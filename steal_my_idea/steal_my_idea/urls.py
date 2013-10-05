7618be71ed6e70adbb6fc3262af2af52e34813ec
jesse-browne <jb@greenpag.es>
2013-10-04 19:08:41 -0700

    Added namespaces to urls

--- steal_my_idea/steal_my_idea/urls.py
+++ steal_my_idea/steal_my_idea/urls.py
@@ -12,8 +12,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^$', include('ideas.urls')),
-    url(r'^ideas/', include('ideas.urls')),
+    #url(r'^$', include('ideas.urls', namespace='ideas')),
+    url(r'^ideas/', include('ideas.urls', namespace='ideas')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
