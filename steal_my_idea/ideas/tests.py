0631380ca655d3d634b4c4ff922c4627484a9a99
jesse-browne <jb@greenpag.es>
2013-10-05 22:32:06 -0700

    Added tests for index view

--- steal_my_idea/ideas/tests.py
+++ steal_my_idea/ideas/tests.py
@@ -6,11 +6,35 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
+from ideas.models import Idea
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def create_idea(title, description):
+    """Create idea with place holder title and description"""
+    return Idea.objects.create(title=title, description=description)
+
+class IdeaViewTests(TestCase):
+    def test_index_view_with_no_ideas(self):
+        """If no ideas show error message"""
+        response = self.client.get(reverse('ideas:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no ideas to steal right now :(')
+        self.assertQuerysetEqual(response.context['latest_idea_list'], [])
+
+    def test_index_view_with_one_idea(self):        
+        create_idea(title='Some idea', description='Lorem ipsum')
+        response = self.client.get(reverse('ideas:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_idea_list'], 
+            ['<Idea: Some idea>']
+        )
+        
+    def test_index_view_with_two_ideas(self):        
+        create_idea(title='Some idea', description='Lorem ipsum')
+        create_idea(title='Another idea', description='Lorem')
+        response = self.client.get(reverse('ideas:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_idea_list'], 
+            ['<Idea: Another idea>', '<Idea: Some idea>']
+        )
