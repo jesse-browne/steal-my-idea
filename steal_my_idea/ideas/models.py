c9c2c0945f4a3fec8e0bb172575596d07c6f103b
jesse-browne <jb@greenpag.es>
2013-10-06 15:06:01 -0700

    Fixed author_wants field to hold a more useful value

--- steal_my_idea/ideas/models.py
+++ steal_my_idea/ideas/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Define models.
 
 class Idea(models.Model):
-    HELP = 'HE'
-    EXECUTION = 'EX'
-    MONEY = 'MO'
+    HELP = 'Help! Work with me to make this idea a reality'
+    EXECUTION = 'Someone, anyone to make take this idea and make it a reality'
+    MONEY = 'Money to take this idea and turn it into a reality'
     AUTHOR_WANTS_OPTIONS = (
-        (HELP, 'Help! Work with me to make this idea a reality'),
+        (HELP,      'Help! Work with me to make this idea a reality'),
         (EXECUTION, 'Someone, anyone to make take this idea and make it a reality'),
-        (MONEY, 'Money to take this idea and turn it into a reality'),            
+        (MONEY,     'Money to take this idea and turn it into a reality'),            
     )
     
     title = models.CharField(max_length=200)
     date_published = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
-    author_wants = models.CharField(max_length=2, choices=AUTHOR_WANTS_OPTIONS, default=EXECUTION)
+    author_wants = models.CharField(max_length=100, choices=AUTHOR_WANTS_OPTIONS, default=EXECUTION)
     votes = models.IntegerField(default=0)
     
     def __unicode__(self):
