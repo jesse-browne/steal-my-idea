18034647166d2cbdb62598b795d2bf4558ce3e02
jesse-browne <jb@greenpag.es>
2013-10-09 13:42:17 -0700

    Show idea author email of they selected help option for author wants.

--- steal_my_idea/ideas/models.py
+++ steal_my_idea/ideas/models.py
@@ -6,19 +6,17 @@ from django.contrib.auth.models import User
 class Idea(models.Model):
     HELP = 'Help! Work with me to make this idea a reality'
     EXECUTION = 'Someone, anyone to make take this idea and make it a reality'
-    MONEY = 'Money to take this idea and turn it into a reality'
     AUTHOR_WANTS_OPTIONS = (
-        (HELP,      'Help! Work with me to make this idea a reality'),
-        (EXECUTION, 'Someone, anyone to make take this idea and make it a reality'),
-        (MONEY,     'Money to take this idea and turn it into a reality'),            
+        (HELP,      'help! Work with me to make this idea a reality, email me to get started.'),
+        (EXECUTION, 'someone, anyone to make take this idea and make it a reality.'),           
     )
     
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, related_name="author", editable=False)
     date_published = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
-    author_wants = models.CharField(max_length=100, choices=AUTHOR_WANTS_OPTIONS, default=EXECUTION)
-    votes = models.IntegerField(default=0)
+    author_wants = models.CharField('I want', max_length=100, choices=AUTHOR_WANTS_OPTIONS, default=EXECUTION)
+    votes = models.IntegerField(default=0, editable=False)
     
     def __unicode__(self):
         return self.title
