39a3f7dd5576359abc0af0b151256040a8c611a1
Jesse Browne <jb@greenpag.es>
2013-10-24 22:25:58 -0700

    Redirect to front end ideas:index.html on successful idea creation.

--- steal_my_idea/ideas/admin.py
+++ steal_my_idea/ideas/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 
 from ideas.models import Idea, Page
 
@@ -17,6 +19,11 @@ class IdeaAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return Idea.objects.all()
         return Idea.objects.filter(author=request.user)
+    
+    def response_add(self, request, obj, post_url_continue=None):
+        """This makes the response after adding go to another apps changelist for some model"""
+        return HttpResponseRedirect(reverse("ideas:index"))
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('heading', 'date_published')
