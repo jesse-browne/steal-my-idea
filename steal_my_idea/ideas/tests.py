81f423bf590aa3623d4ea08b1e881d3d0b26cfca
Jesse Browne <jb@greenpag.es>
2013-11-09 15:29:28 -0800

    Fixed broken test_index_view_with_one_idea

--- steal_my_idea/ideas/tests.py
+++ steal_my_idea/ideas/tests.py
@@ -5,12 +5,14 @@ These will pass when you run "manage.py test".
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.contrib.auth.models import User
 
 from ideas.models import Idea
 
-def create_idea(title, description):
+def create_idea(title, description, author):
     """Create idea with place holder title and description"""
-    return Idea.objects.create(title=title, description=description)
+    return Idea.objects.create(title=title, description=description, author=author)
 
 def create_page(heading, text):
     """Create a page with heading and text"""
@@ -24,13 +26,14 @@ class IdeaViewTests(TestCase):
         self.assertContains(response, 'There are no ideas to steal right now :(')
         self.assertQuerysetEqual(response.context['latest_idea_list'], [])
 
-#    def test_index_view_with_one_idea(self):        
-#        create_idea(title='Some idea', description='Lorem ipsum')
-#        response = self.client.get(reverse('ideas:index'))
-#        self.assertQuerysetEqual(
-#            response.context['latest_idea_list'], 
-#            ['<Idea: Some idea>']
-#        )
+    def test_index_view_with_one_idea(self):
+        new_user = User.objects.create_user('Timmy', 'timmy@jimmy.com', 'timmyspwd')
+        create_idea(title='Some idea', description='Lorem ipsum', author=new_user)
+        response = self.client.get(reverse('ideas:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_idea_list'], 
+            ['<Idea: Some idea>']
+        )
         
 #    def test_index_view_with_two_ideas(self):        
 #        create_idea(title='Some idea', description='Lorem ipsum')
